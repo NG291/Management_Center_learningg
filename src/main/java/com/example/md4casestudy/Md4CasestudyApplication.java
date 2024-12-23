2e90f5f15a8fef39d34cbd500d202317efe69783
NG291 <you@example.com>
2024-12-23 09:59:35 +0700

    update

---
+++ src/main/java/com/example/md4casestudy/Md4CasestudyApplication.java
@@ -0,0 +1,15 @@
+package com.example.md4casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class Md4CasestudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4CasestudyApplication.class, args);
+    }
+
+}
